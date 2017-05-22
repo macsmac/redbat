ba79b69a2355a08f1c5ccdb38278f9e7e68815b5
Gleb Makagonov <bywildeme@gmail.com>
2017-05-22 17:08:05 +0500

    added event piping

--- src/eventemitter.js
+++ src/eventemitter.js
@@ -3,6 +3,7 @@ const Namespace = require("./namespace");
 module.exports = function() {
 	this.DEFAULT_NAMESPACE = "default";
 	this.namespaces = {};
+	this.connected = [];
 
 	this.namespace = function(name) {
 		if (!name) return this.namespaces[this.DEFAULT_NAMESPACE];
@@ -18,9 +19,15 @@ module.exports = function() {
 
 	this.on = this.namespace().on;
 	this.once = this.namespace().once;
-	this.emit = this.namespace().emit;
+	this.emit = function() {
+		this.connected.forEach(e => e.emit.apply(e, arguments));
+		return this.namespace().emit.apply(this, arguments);
+	};
 	this.use = this.namespace().use;
 	this.listener = this.namespace().getListeners;
+	this.pipe = function(emitter) {
+		this.connected.push(emitter);
+	}
 
 	return this;
 }
